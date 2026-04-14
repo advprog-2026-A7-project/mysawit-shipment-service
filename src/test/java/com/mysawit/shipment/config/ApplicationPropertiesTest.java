ff47f2e267b2b601c5fa5bb64c44974bbc3a2420
azzelll <madeshandykrisnanda@gmail.com>
2026-04-14 23:02:27 +0700

    [REFACTOR] tidy rabbit health configuration test

--- src/test/java/com/mysawit/shipment/config/ApplicationPropertiesTest.java
+++ src/test/java/com/mysawit/shipment/config/ApplicationPropertiesTest.java
@@ -10,15 +10,25 @@ import org.junit.jupiter.api.Test;
 
 class ApplicationPropertiesTest {
 
+    private static final String APPLICATION_PROPERTIES = "application.properties";
+    private static final String RABBIT_HEALTH_PROPERTY = "management.health.rabbit.enabled";
+
     @Test
     void applicationPropertiesDisablesRabbitHealthIndicator() throws IOException {
-        try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream("application.properties")) {
+        Properties properties = loadApplicationProperties();
+
+        assertEquals("false", properties.getProperty(RABBIT_HEALTH_PROPERTY));
+    }
+
+    private Properties loadApplicationProperties() throws IOException {
+        try (InputStream inputStream = Thread.currentThread()
+                .getContextClassLoader()
+                .getResourceAsStream(APPLICATION_PROPERTIES)) {
             assertNotNull(inputStream);
 
             Properties properties = new Properties();
             properties.load(inputStream);
-
-            assertEquals("false", properties.getProperty("management.health.rabbit.enabled"));
+            return properties;
         }
     }
 }
