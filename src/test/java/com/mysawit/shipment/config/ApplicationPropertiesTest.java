68231881a2bbd0d122592f6712c5a65943ae2efe
azzelll <madeshandykrisnanda@gmail.com>
2026-04-18 01:26:39 +0700

    Use EC2 host networking for Supabase Direct IPv6, simplify deploy envs, and improve health/debug check

--- src/test/java/com/mysawit/shipment/config/ApplicationPropertiesTest.java
+++ src/test/java/com/mysawit/shipment/config/ApplicationPropertiesTest.java
@@ -12,12 +12,21 @@ class ApplicationPropertiesTest {
 
     private static final String APPLICATION_PROPERTIES = "application.properties";
     private static final String RABBIT_HEALTH_PROPERTY = "management.health.rabbit.enabled";
+    private static final String READINESS_GROUP_PROPERTY =
+            "management.endpoint.health.group.readiness.include";
 
     @Test
     void applicationPropertiesDisablesRabbitHealthIndicator() throws IOException {
         Properties properties = loadApplicationProperties();
 
         assertEquals("false", properties.getProperty(RABBIT_HEALTH_PROPERTY));
+    }
+
+    @Test
+    void applicationPropertiesIncludeDatabaseInReadinessHealth() throws IOException {
+        Properties properties = loadApplicationProperties();
+
+        assertEquals("readinessState,db", properties.getProperty(READINESS_GROUP_PROPERTY));
     }
 
     private Properties loadApplicationProperties() throws IOException {
